170d4d7fb38600eef3a5d450970732a2fbcd7bc3
maayanfi <maayanfi@gmail.com>
2026-05-19 17:06:48 +0300

    logs

--- BaseBin/libjailbreak/src/jbclient_xpc.c
+++ BaseBin/libjailbreak/src/jbclient_xpc.c
@@ -10,12 +10,10 @@
 #include <os/alloc_once_private.h>
 
 #include "roothider/log.h"
-#ifdef ENABLE_LOGS
 void (*XPCLogDebugFunction)(const char *format, ...);
 void (*XPCLogErrorFunction)(const char *format, ...);
 #define JBLogDebug(...) do { if(XPCLogDebugFunction)XPCLogDebugFunction(__VA_ARGS__); } while(0)
 #define JBLogError(...) do { if(XPCLogErrorFunction)XPCLogErrorFunction(__VA_ARGS__); } while(0)
-#endif
 
 struct xpc_global_data {
 	uint64_t    a;
